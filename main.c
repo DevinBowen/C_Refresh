9c109c90d842469ff4e74795f416dc19964efcf2
devin <bowen61496@gmail.com>
2023-07-24 11:21:34 -0400

    1

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int numberOfArguments = argc
+    char *argument1 = argv[0];
+    char *argument2 = argv[1];
+
+    printf("Hello World");
+    enum color {red, blue, green};
+    enum color devin;
+    devin = blue;
+    printf("%d",devin);
+
+    return 0;
+}
