1ce7d67049741039af512936e3afa6a6e657ed37
devin <bowen61496@gmail.com>
2023-07-24 14:32:48 -0400

    pointers

--- main.c
+++ main.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
 
+void point(){
+    printf("A lot of tests.\n");
+    
+    int num = 0;
+    int *pnum;
+    
+    num = 10;
+    pnum = &num;
+    printf("pnum address: %p\n", pnum);
+    printf("pnum: %d\n", *pnum);
+    printf("num address: %p\n", &num);
+    printf("pnum address: %p\n", (void*)&pnum);
+    printf("pnum size: %d bytes\n", (int)sizeof(pnum));
+    
+}
+
 int main(int argc, char *argv[])
 {
-    int numberOfArguments = argc
+    point();
+    
+    int numberOfArguments = argc;
     char *argument1 = argv[0];
     char *argument2 = argv[1];
-
-    printf("Hello World");
-    enum color {red, blue, green};
-    enum color devin;
-    devin = blue;
-    printf("%d",devin);
-
     return 0;
 }
